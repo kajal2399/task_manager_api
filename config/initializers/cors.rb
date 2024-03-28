b443bb8cdfde8145a43655c1a360cee3dd4b54c3
kajal2399 <kajalsharma7688@gmail.com>
2024-03-28 14:16:29 +0530

    gave access to the FE URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000'
+      origins 'http://localhost:3000', 'https://amazing-chebakia-476b39.netlify.app'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
     end
 end
