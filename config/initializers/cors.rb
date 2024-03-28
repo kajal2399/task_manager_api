6254f0ef9f28af07945d75d67d26af81c5ef3c13
kajal2399 <kajalsharma7688@gmail.com>
2024-03-28 15:33:05 +0530

    minor fix for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
-             credentials: false
+             credentials: false,
+             expose: ['Authorization', 'Token'], # Include any other headers that need to be exposed
+             max_age: 600 # Optional; set this to the number of seconds the preflight OPTIONS response should be cached
   end
 end
