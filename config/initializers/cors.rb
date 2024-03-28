70aa37632340a76250bc45eb24674b16c4e6f995
kajal2399 <kajalsharma7688@gmail.com>
2024-03-28 14:51:09 +0530

    minor fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://amazing-chebakia-476b39.netlify.app'
+      origins '*'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
     end
 end
+
